5f9c26489980aeee72afbbc6aaa3a73dd1e39b10
david <davidabu91@gmail.com>
2020-06-17 12:50:19 +0300

    Updates

---
+++ precache-manifest.22b6a21bfc04c9fb9baeb77f700ca9f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e420d33eeaea1db7e7de5df824900d",
+    "url": "/david-abou-17-06-2020%20/index.html"
+  },
+  {
+    "revision": "a4f16af4ad1ce03a888e",
+    "url": "/david-abou-17-06-2020%20/static/css/main.e84db866.chunk.css"
+  },
+  {
+    "revision": "9187209f371845a882fc",
+    "url": "/david-abou-17-06-2020%20/static/js/2.26424511.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/david-abou-17-06-2020%20/static/js/2.26424511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f16af4ad1ce03a888e",
+    "url": "/david-abou-17-06-2020%20/static/js/main.2e3e5f20.chunk.js"
+  },
+  {
+    "revision": "5195d697e71fc85bc32a",
+    "url": "/david-abou-17-06-2020%20/static/js/runtime-main.00df18f0.js"
+  }
+]);
